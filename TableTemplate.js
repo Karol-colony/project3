f0efe76882ac0f1037bf9a3688f3a7948b927f4a
pat harrington <harrin7@gmail.com>
2017-02-04 19:38:15 -0800

    finished problem: filled in cells correctly

--- TableTemplate.js
+++ TableTemplate.js
@@ -1,24 +1,23 @@
 class TableTemplate {
 	static fillIn(id, dic, columnName) {
-		var columnName = columnName;
-		console.log(columnName);
-		var dic = dic;
-		console.log(dic);
 		const table = document.getElementById(id);
-		console.log(table);
 
 		// get lenght of first row
-		var c = document.getElementById(id).rows[0].cells.length - 1; // number of columns, zero indexed
-		for (let y = 0; y <= 4; y++) {
-			for (let z = 0; z <= c; z++) {
-				var template = document.getElementById(id).rows[y].cells[z].innerHTML;
-				var dateTemplate = new Cs142TemplateProcessor(template);
-				var str = dateTemplate.fillIn(dic);
-
-				document.getElementById(id).rows[y].cells[z].innerHTML = str;
-			}
+		var c = document.getElementById(id).rows[0].cells.length - 1; // number of columns, zero indexed	
+		for (let z = 0; z <= c; z++) {
+				let template = document.getElementById(id).rows[0].cells[z].innerHTML;
+				let dateTemplate = new Cs142TemplateProcessor(template);
+				let str = dateTemplate.fillIn(dic);
+				document.getElementById(id).rows[0].cells[z].innerHTML = str;
+				if (columnName === str || columnName === undefined){
+					for (let y = 1; y <= 4; y++) {
+						template = document.getElementById(id).rows[y].cells[z].innerHTML;
+						dateTemplate = new Cs142TemplateProcessor(template);
+						str = dateTemplate.fillIn(dic);
+						document.getElementById(id).rows[y].cells[z].innerHTML = str;
+					}
+				}
 		}
-
 
         table.style.visibility = "visible";
 	}
