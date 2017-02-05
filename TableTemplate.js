77d97a1bc61c6b0af4e0a18e131bc2caab111b3e
pat harrington <harrin7@gmail.com>
2017-02-04 18:31:10 -0800

    fill in all cells with dictionary objects

--- TableTemplate.js
+++ TableTemplate.js
@@ -1,11 +1,25 @@
 class TableTemplate {
 	static fillIn(id, dic, columnName) {
-		var table = document.getElementById(id);
-		table.style.visibility = "visible";
-		console.log(table);
 		var columnName = columnName;
 		console.log(columnName);
-		var template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
-        var dateTemplate = new Cs142TemplateProcessor(template);
+		var dic = dic;
+		console.log(dic);
+		const table = document.getElementById(id);
+		console.log(table);
+
+		// get lenght of first row
+		var c = document.getElementById(id).rows[0].cells.length - 1; // number of columns, zero indexed
+		for (let y = 0; y <= 4; y++) {
+			for (let z = 0; z <= c; z++) {
+				var template = document.getElementById(id).rows[y].cells[z].innerHTML;
+				var dateTemplate = new Cs142TemplateProcessor(template);
+				var str = dateTemplate.fillIn(dic);
+
+				document.getElementById(id).rows[y].cells[z].innerHTML = str;
+			}
+		}
+
+
+        table.style.visibility = "visible";
 	}
 }
